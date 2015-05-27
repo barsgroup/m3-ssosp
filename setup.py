f69b376f8054fbcccd15d58510ba7da83e73d360
Максим Елахов <elakhov@bars-open.ru>
2015-05-27 08:58:33 +0400

    1.1.4 Добавлен changelog

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 
+
 def read(fname):
     try:
-        return open(os.path.join(os.path.dirname(__file__),
-            fname)).read()
+        return open(
+            os.path.join(os.path.dirname(__file__), fname)
+        ).read()
     except IOError:
         return ''
 
-setup(name='m3-ssosp',
-      version='1.1.3',
-      url='https://bitbucket.org/barsgroup/ssosp',
-      license='MIT',
-      author='BARS Group',
-      author_email='kirov@bars-open.ru',
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      description=u'Single Sign-On Service Provider',
-      install_requires=read('REQUIREMENTS'),
-      include_package_data=True,
-      classifiers=[
+setup(
+    name='m3-ssosp',
+    version='1.1.4',
+    url='https://bitbucket.org/barsgroup/ssosp',
+    license='MIT',
+    author='BARS Group',
+    author_email='kirov@bars-open.ru',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    description=u'Single Sign-On Service Provider',
+    install_requires=read('REQUIREMENTS'),
+    include_package_data=True,
+    classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Natural Language :: Russian',
@@ -29,5 +32,5 @@ setup(name='m3-ssosp',
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
-      ],
-      )
+    ],
+)
