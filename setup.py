6006a6bc605ed349d727cba0f12e68cbc52db8df
Илья Киров <kirov@bars-open.ru>
2018-05-07 17:26:02 +0300

    Из-за tuple не загружался на pypi

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     ),
     long_description=_read('README.md'),
     include_package_data=True,
-    classifiers=(
+    classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Natural Language :: Russian',
@@ -40,8 +40,8 @@ setup(
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
-    ),
+    ],
     dependency_links=('http://pypi.bars-open.ru/simple/m3-builder',),
-        setup_requires=('m3-builder>=1.0.1',),
+    setup_requires=('m3-builder>=1.0.1',),
     set_build_info=os.path.dirname(__file__),
 )
